cbb653bc9a21811c0f6e9d4bddeabb0f9b075678
Kauan Victor <kauanvictormelo06@gmail.com>
2024-10-25 13:26:26 -0300

    tesde de js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var card = document.getElementsByClassName('foto');
+var cards = document.getElementsByClassName('foto');
 
-card.addEventListener('touchStart', function() {
-    card.classList.add('active')
-})
-card.addEventListener('touchedn', function() {
-    card.classList.remove('active')
-})
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('touchstart', function() {
+        this.classList.add('active');
+    });
+
+    cards[i].addEventListener('touchend', function() {
+        this.classList.remove('active');
+    });
+}
